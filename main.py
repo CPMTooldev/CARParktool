527cd6963b57c3ddd27459b7207c5dbf1a383383
CPMTooldev <telmnbattr@gmail.com>
2025-02-08 05:15:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -606,7 +606,7 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.rainbow, 'FAILED.'))
                     print(Colorate.Horizontal(Colors.rainbow, '[!] Please use valid values.'))
                     sleep(2)
-                    continue       
+                    continue
               else: continue 
                 break
               break
