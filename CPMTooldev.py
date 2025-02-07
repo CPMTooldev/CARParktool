f70bc8e2f665c972bf5ae96978b0fa79149bdac6
CPMTooldev <telmnbattr@gmail.com>
2025-02-07 18:04:49 +0800

    CPMTooldev.py

--- CPMTooldev.py
+++ CPMTooldev.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://telmunnshop.squareweb.app/api"
 
-class CPMTooldevz:
+class CPMTooldev:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
