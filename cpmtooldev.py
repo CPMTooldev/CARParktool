0b5869bd8df54ac0c83dfa319bdb65bdd7031901
CPMTooldev <telmnbattr@gmail.com>
2025-02-08 05:28:36 +0800

    cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -221,5 +221,13 @@ class CPMTooldev:
         response = requests.post(f"{BASE_URL}/clone", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
-
+    def hack_car_speed(self, car_id):
+        payload = {
+            "account_auth": self.auth_token,
+            "car_id": car_id
+        }
+        params = { "key": self.access_key }
+        response = requests.post(f"{BASE_URL}/hack_car_speed", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
     
