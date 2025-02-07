2216827a5d8de31d0b53c5e9db0457587c105cc1
CPMTooldev <telmnbattr@gmail.com>
2025-02-08 05:29:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -606,6 +606,14 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.rainbow, '[!] Please use valid values.'))
                     sleep(2)
                     continue
-            else: continue
-            break
-        break
+            elif service == 27: # Hack Car Speed (299hp)
+                console.print("[bold yellow][!] Note[/bold yellow]: original speed can not be restored !.")
+                console.print("[bold cyan][!] Enter Car Details.[/bold cyan]")
+                car_id = IntPrompt.ask("[bold][?] Car ID[/bold]")
+                console.print("[bold cyan][%] Hacking Car Speed[/bold cyan]: ", end=None)
+                if cpm.hack_car_speed(car_id):
+                    console.print("[bold green]SUCCESSFUL (✔)[/bold green]")
+                    console.print("==================================")
+                    answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
+                    if answ == "y": console.print("[bold yellow][!] Thank You for using our tool.[/bold yellow].")
+                    else: continue
