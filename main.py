a3ddb8e6344dd95296b7c99f1d379537b52050c2
CPMTooldev <telmnbattr@gmail.com>
2025-02-08 06:23:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -605,17 +605,6 @@ if __name__ == "__main__":
                         
                     print(Colorate.Horizontal(Colors.rainbow, 'FAILED.'))
                     print(Colorate.Horizontal(Colors.rainbow, '[!] Please use valid values.'))
-                    continue
-            elif service == 27: # Hack Car Speed (299hp)
-                console.print("[bold yellow][!] Note[/bold yellow]: original speed can not be restored !.")
-                console.print("[bold cyan][!] Enter Car Details.[/bold cyan]")
-                account_get_car = IntPrompt.ask("[bold][?] Car ID[/bold]")
-                console.print("[bold cyan][%] Hacking Car Speed[/bold cyan]: ", end=None)
-                if cpm.account_hack_car_speed(cadid):
-                    console.print("[bold green]SUCCESSFUL (✔)[/bold green]")
-                    console.print("==================================")
-                    answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
-                    if answ == "y": console.print("[bold yellow][!] Thank You for using our tool.[/bold yellow].")
                     sleep(2)
                     continue
             else: continue
