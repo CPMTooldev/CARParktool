e107cacda81da478ce234ce5d1f1e4f3a8ed901d
CPMTooldev <telmnbattr@gmail.com>
2025-02-10 12:56:47 +0800

    Update and rename cpmtooldev1.py to cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -216,3 +216,13 @@ class CPMTooldev:
         response = requests.post(f"{BASE_URL}/unlock_equipments_female", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
+
+    def hack_car_speed(self, car_id):
+        payload = {
+            "account_auth": self.auth_token,
+            "car_id": car_id
+        }
+        params = { "key": self.access_key }
+        response = requests.post(f"{__ENDPOINT_URL__}/hack_car_speed", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
