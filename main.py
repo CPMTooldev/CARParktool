49c736e54497ecaa9d05cc419b04a92d794f7496
CPMTooldev <telmnbattr@gmail.com>
2025-03-05 03:00:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{26}: Clone Account            7K'))
             print(Colorate.Horizontal(Colors.rainbow, '{27}: Glitch car               2.5k'))
             print(Colorate.Horizontal(Colors.rainbow, '{28}: Custom Angle             1.5k'))
-            print(Colorate.Horizontal(Colors.rainbow, '{28}: Custom Tire burner       1.5k'))
+            print(Colorate.Horizontal(Colors.rainbow, '{29}: Custom Tire burner       1.5k'))
             print(Colorate.Horizontal(Colors.rainbow, '{0} : Exit'))
             
             print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐂𝐏𝐌 ]==============='))
