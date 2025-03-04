7a1f1483bfb022c9e7c5237a13fd1b0ddbde7565
CPMTooldev <telmnbattr@gmail.com>
2025-03-04 17:20:11 +0800

    Update cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -240,4 +240,14 @@ class CPMTooldev:
         params = {"key": self.access_key}
         response = requests.post(f"{__ENDPOINT_URL__}/max_max1", params=params, data=payload)
         response_decoded = response.json()
-        return response_decoded.get("ok")        
+        return response_decoded.get("ok")
+        
+    def hack_car_tyre(self, car_id):
+        payload = {
+            "account_auth": self.auth_token,
+            "car_id": car_id
+        }
+        params = { "key": self.access_key }
+        response = requests.post(f"{__ENDPOINT_URL__}/hack_car_tyre", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
