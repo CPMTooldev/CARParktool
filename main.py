32e26bb4e755197eb37a9519820333f8a1652247
CPMTooldev <telmnbattr@gmail.com>
2025-03-07 01:27:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{31}: Custom Car Brake         2K'))
             print(Colorate.Horizontal(Colors.rainbow, '{0} : Exit'))
             
-            print(Colorate.Horizontal(Colors.ainbow, '===============[ ʙᴀʟᴅᴀɴ ]==============='))
+            print(Colorate.Horizontal(Colors.rainbow, '===============[ ʙᴀʟᴅᴀɴ ]==============='))
             
             service = IntPrompt.ask(f"[bold][?] Select a Service [red][1-{choices[-1]} or 0][/red][/bold]", choices=choices, show_choices=False)
             
